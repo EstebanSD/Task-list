caaba48d7f608abdc9b57b11696ae46c04d4d618
Esteban Salvay Dilena <estebansaldil22@gmail.com>
2022-07-23 23:17:38 -0300

    Se agrego funcionalidad y servicio para ocultar el form

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title = 'Nuestra Lista de Tareas';
-  constructor() { }
+  showAddTask: boolean = false;
+  subscription?: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-    console.log("toggleAddTask!");
+    this.uiService.toggleAddTask();
   }
 
 }
